7cb74af7bf1cd6d4459c576dcce9b84189d819e4
ich5nkn <kogaktt@gmail.com>
2023-02-11 18:59:47 +0900

    fix: move source file to src

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const fetchApi = async () => {
+    const res = await fetch("/api/hello");
+    const json = await res.json();
+    console.log(json);
+  };
+
   return (
     <>
       <Head>
@@ -26,7 +32,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -57,6 +63,7 @@ export default function Home() {
               priority
             />
           </div>
+          <button onClick={fetchApi}>click</button>
         </div>
 
         <div className={styles.grid}>
@@ -119,5 +126,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
