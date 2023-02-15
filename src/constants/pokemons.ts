d0f8690d897b7be1d45c5ebc686b3993cbdab511
ich5nkn <kogaktt@gmail.com>
2023-02-15 20:45:03 +0900

    move: test pokemon data

---
+++ src/constants/pokemons.ts
@@ -0,0 +1,7 @@
+export const POKEMONS = [
+  { id: "1", name: "フシギダネ" },
+  { id: "2", name: "フシギソウ" },
+  { id: "3", name: "フシギバナ" },
+  { id: "3-1", name: "メガフシギバナ" },
+  { id: "3-2", name: "フシギバナ（キョダイマックスのすがた）" },
+] as const;
