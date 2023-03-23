3a37b81552f9d6f884d95cf9240b07b81e73e721
ich5nkn <kogaktt@gmail.com>
2023-03-23 22:53:19 +0900

    feat: option check logic

--- src/pages/select.tsx
+++ src/pages/select.tsx
@@ -1,11 +1,36 @@
-import { useState } from "react";
-import { BallCard } from "@/components/pages/index/BallCard";
-import { SettingCard } from "@/components/pages/index/SettingCard";
-import { SettingModal } from "@/components/pages/index/SettingModal";
+import { useReducer, useState } from "react";
+import { BallCard } from "@/components/pages/select/BallCard";
+import { SettingCard } from "@/components/pages/select/SettingCard";
+import { SettingModal } from "@/components/pages/select/SettingModal";
 import { Grid, Heading } from "@chakra-ui/react";
+import { VERSIONS } from "@/constants/version";
+import { SettingOptions } from "@/types";
+
+export type OptionsType = {
+  versions: SettingOptions;
+};
+
+export type Action = {
+  type: "versions";
+  value: SettingOptions;
+};
+
+const initialOptions: OptionsType = {
+  versions: VERSIONS.map(({ id, name }) => ({ id, name, value: false })),
+};
+
+const optionsReducer = (options: OptionsType, action: Action): OptionsType => {
+  switch (action.type) {
+    case "versions":
+      return { ...options, versions: action.value };
+    default:
+      return options;
+  }
+};
 
 const Select = () => {
   const [open, setOpen] = useState(false);
+  const [options, dispatch] = useReducer(optionsReducer, initialOptions);
 
   return (
     <Grid gap={8} my={12}>
@@ -21,7 +46,12 @@ const Select = () => {
           setOpen(true);
         }}
       />
-      <SettingModal open={open} onClose={() => setOpen(false)} />
+      <SettingModal
+        open={open}
+        onClose={() => setOpen(false)}
+        options={options}
+        dispatch={dispatch}
+      />
     </Grid>
   );
 };
