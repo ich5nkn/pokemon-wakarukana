0944795f5a50ed3de713864d2bb64e614262fbbb
ich5nkn <kogaktt@gmail.com>
2023-03-26 23:09:40 +0900

    feat: customize switch design

--- src/components/pages/select/SettingModal.tsx
+++ src/components/pages/select/SettingModal.tsx
@@ -1,12 +1,17 @@
 import { Action, OptionsType } from "@/pages/select";
 import {
   Button,
+  Flex,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalFooter,
   ModalHeader,
+  Spacer,
+  Switch,
+  Text,
+  VStack,
 } from "@chakra-ui/react";
 import { Dispatch } from "react";
 import { Options } from "./Options";
@@ -33,11 +38,43 @@ export const SettingModal = ({
         <ModalHeader>難易度カスタマイズ</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
+          {/* TODO: 共通化 */}
+          <Text fontSize={"lg"} fontWeight={700} my={2}>
+            出題形式
+          </Text>
+          <VStack spacing={2} align={"left"} my={2}>
+            <Flex>
+              4択で出題する
+              <Spacer /> <Switch />
+            </Flex>
+            <Flex>
+              シルエットで出題する
+              <Spacer /> <Switch disabled />
+            </Flex>
+          </VStack>
+          <Text fontSize={"lg"} fontWeight={700} my={2}>
+            出題範囲
+          </Text>
+          <VStack spacing={2} align={"left"} my={2}>
+            <Flex>
+              リージョンフォームを含む
+              <Spacer /> <Switch />
+            </Flex>
+            <Flex>
+              メガシンカを含む
+              <Spacer /> <Switch />
+            </Flex>
+            <Flex>
+              キョダイマックスを含む
+              <Spacer /> <Switch />
+            </Flex>
+          </VStack>
           <Options
-            title="初登場シリーズ"
+            title="初登場シリーズで絞り込む"
             options={options.versions}
             updateOptions={updateVersion}
           />
+          {/* TODO: 問題数の選択 */}
         </ModalBody>
         <ModalFooter columnGap={4}>
           <Button colorScheme={"orange"} onClick={onClose}>
