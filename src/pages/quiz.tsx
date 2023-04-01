84ac1e2376c4ac77a489bc7de2caab82f21ec1c1
ich5nkn <kogaktt@gmail.com>
2023-04-01 15:54:57 +0900

    feat: copy top page to quiz

---
+++ src/pages/quiz.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Image from "next/image";
+import { getQuiz } from "@/utils/fetcher";
+import { QuizOption } from "@/types";
+import { Box, Button } from "@chakra-ui/react";
+
+const Quiz = () => {
+  const [no, setNo] = useState<string>("1");
+  const [answered, setAnswered] = useState<string[]>(["1"]);
+  const [finished, setFinished] = useState<boolean>(false);
+  const onClick = async () => {
+    try {
+      const dummyOptions: QuizOption = {
+        isSelectableQuiz: false,
+        maxCount: 3,
+        selectVersions: [],
+      };
+      const res = await getQuiz({ answered, option: dummyOptions });
+      if (res.finished) return setFinished(res.finished);
+      if (!res.no) return;
+      // TODO: 回答のリクエスト送信処理を作成したら、そちらに移動する（現在は無いので、問題を受け取ったら回答したものとしている）
+      setAnswered([...answered, res.no]);
+      setNo(res.no);
+    } catch {
+      alert("error");
+    }
+  };
+
+  return (
+    <Box display={"flex"} m={5}>
+      {finished ? (
+        "Finished!"
+      ) : (
+        <>
+          <Image
+            src={`/image/pokemon/${no}.png`}
+            alt="pokemon image"
+            width={100}
+            height={100}
+          />
+          <Button onClick={onClick}>Change</Button>
+        </>
+      )}
+    </Box>
+  );
+};
+
+export default Quiz;
