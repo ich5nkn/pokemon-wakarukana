d5458f8310d8ed7ac06cd160635a33af7e657d5d
ich5nkn <kogaktt@gmail.com>
2023-04-08 15:56:59 +0900

    add: shuffle function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,12 @@ export const randomPick = <T>(arr: Array<T>): T | undefined => {
   if (!arr.length) return;
   return arr[Math.floor(Math.random() * arr.length)];
 };
+
+export const shuffleArray = <T extends Array<any>>(array: T): T => {
+  const shuffledArray: T = [...array] as T;
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
+};
