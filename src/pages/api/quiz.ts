7842d3335ec2ce38fd0474cfc8008c91d8e85804
ich5nkn <kogaktt@gmail.com>
2023-04-10 20:38:59 +0900

    chore: add comments analyzing current process and outlining plan for improvement

--- src/pages/api/quiz.ts
+++ src/pages/api/quiz.ts
@@ -15,18 +15,40 @@ export default function handler(
 ) {
   const { displayed, options, answer } = req.body;
 
+  /**
+   * [ 新しい処理の流れ ]
+   * 
+   * - 共通処理
+   *   - POKEMON から option で filter する
+   *   - filter された option を 回答済みと未回答で分ける
+   * 
+   * - 正解・不正解の判定
+   *   - 回答済みの最後の pokemon を前回の問題の答えとして、成否判定と回答の返却を行う
+   * 
+   * - 次の問題作成
+   *   - 未回答の pokemon から random で 1つ Pick し、次の問題とする
+   *   - isChoice のとき、上記を除外した pokemon から random で 3つ Pick し、ダミーの選択肢とする
+   * 
+   */
+
+
+
+  // POKEMON から options で filter, displayed で filter して random pick
   const nextPokemon = getNextPokemon(displayed, options);
   if (options.numberOfQuiz <= displayed.length || !nextPokemon)
     return res.status(200).json({ finished: true });
 
+  // POKEMON から options ( version 除く ) で filter, 正解と同じ pokemon も filter して random multi pick
   const selector = options.isChoice
     ? getSelector(options, nextPokemon?.no)
     : undefined;
 
+  // POKEMON から target の pokemon を filter して、答え合わせ
   const isCorrect = answer
     ? judgeAnswer(displayed[displayed.length - 1], answer.name, answer.name2)
     : undefined;
 
+  // POKEMON から target の pokemon を filter して、答えを抽出
   const correctAnswer = ((): Answer | undefined => {
     if (!displayed.length || isCorrect) return;
     const targetPokemon = POKEMONS.filter(
