9bf0b61e9aaa2399377cff8ab7ffcea48319ab7d
ich5nkn <kogaktt@gmail.com>
2023-04-16 00:50:30 +0900

    add: public

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -3,5 +3,6 @@ import type { NextApiResponse } from "next";
 export default function handler(_req: null, res: NextApiResponse) {
   res.status(200).json({
     cwd: process.cwd(),
+    public: process.env.NEXT_PUBLIC_BASE_URL,
   });
 }
