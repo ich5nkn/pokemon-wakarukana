0b1e77966a64114381bc1464ee348807576c29a8
ich5nkn <kogaktt@gmail.com>
2023-04-16 01:00:23 +0900

    add: vercel url

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -3,6 +3,7 @@ import type { NextApiResponse } from "next";
 export default function handler(_req: null, res: NextApiResponse) {
   res.status(200).json({
     cwd: process.cwd(),
-    public: process.env.NEXT_PUBLIC_BASE_URL,
+    public: process.env.NEXT_PUBLIC_BASE_URL || "nothing",
+    base: process.env.VERCEL_URL,
   });
 }
