be8008299fb5266d2f18ccd0e20cfe70ffef0204
ich5nkn <kogaktt@gmail.com>
2023-04-16 14:29:53 +0900

    feat: check query

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -3,6 +3,24 @@ import { OptionsType } from "@/types";
 import { AnyObject } from "@/types/utils";
 import queryString from "querystring";
 
+export type validQuery = { [k in keyof OptionsType]: string } & {
+  versions: string | string[];
+};
+
+export const checkQuery = (query: AnyObject): query is OptionsType => {
+  return (
+    "numberOfQuiz" in query &&
+    "isChoice" in query &&
+    "showHint" in query &&
+    "isSilhouette" in query &&
+    "hasRegion" in query &&
+    "hasAnotherForm" in query &&
+    "hasMega" in query &&
+    "hasGigantic" in query &&
+    "versions" in query
+  );
+};
+
 export const optionsToQuery = (options: OptionsType) => {
   return queryString.stringify({
     ...options,
@@ -10,8 +28,11 @@ export const optionsToQuery = (options: OptionsType) => {
   });
 };
 
-export const queryToOptions = (query: AnyObject): OptionsType => {
+export const queryToOptions = (query: validQuery): OptionsType => {
   try {
+    const versions = Array.isArray(query.versions)
+      ? query.versions
+      : [query.versions];
     const options = {
       numberOfQuiz: Number(query.numberOfQuiz),
       isChoice: query.isChoice === "true",
@@ -23,7 +44,7 @@ export const queryToOptions = (query: AnyObject): OptionsType => {
       hasGigantic: query.hasGigantic === "true",
       versions: VERSIONS.map((version) => ({
         id: version.id,
-        value: query.versions.includes(String(version.id)),
+        value: versions.includes(String(version.id)),
       })),
     };
     return options;
