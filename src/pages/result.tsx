06e76b7865f4082117417b564f15f0091d15dc91
ich5nkn <kogaktt@gmail.com>
2023-04-23 01:06:35 +0900

    feat: base result page

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -1,7 +1,23 @@
+import { useMemo } from "react";
 import { Box } from "@chakra-ui/react";
+import { useGlobalState } from "@/hooks/useGlobalState";
+import { BALLS_CONTENT } from "@/constants/balls";
 
 const Result = () => {
-  return <Box>finished!</Box>;
+  const { globalState } = useGlobalState();
+  const selectedOptionType = useMemo(() => {
+    if (!globalState.options?.selectedOptionType) return null;
+    if (globalState.options.selectedOptionType === "custom")
+      return "カスタム難易度";
+    return BALLS_CONTENT[globalState.options.selectedOptionType].name;
+  }, [globalState.options]);
+  return (
+    <Box>
+      <Box>{selectedOptionType}</Box>
+      <Box>{`正解数：${globalState.answered.correct}`}</Box>
+      <Box>{`不正解数：${globalState.answered.incorrect}`}</Box>
+    </Box>
+  );
 };
 
 export default Result;
