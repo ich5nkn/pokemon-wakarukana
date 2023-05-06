2f6781643131b1037f9b28c7c91cc1624c5fc036
ich5nkn <kogaktt@gmail.com>
2023-05-06 18:45:15 +0900

    fix: move placeholder

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,25 +4,29 @@ import { HamburgerIcon } from "@chakra-ui/icons";
 
 export const Header = () => {
   return (
-    <Box
-      h={12}
-      p={2}
-      position="fixed"
-      top={0}
-      left={0}
-      right={0}
-      mx={"auto"}
-      justifyContent={"space-between"}
-      display={"flex"}
-      maxW={"sm"}
-      zIndex={1}
-      bgColor={"white"}
-    >
-      <Text className={styles.header_title}>ポケモンわかるかな?</Text>
-      <HamburgerIcon
-        boxSize={8}
-        // TODO: Menu の実装
-      />
-    </Box>
+    <>
+      <Box
+        h={12}
+        p={2}
+        position="fixed"
+        top={0}
+        left={0}
+        right={0}
+        mx={"auto"}
+        justifyContent={"space-between"}
+        display={"flex"}
+        maxW={"sm"}
+        zIndex={1}
+        bgColor={"white"}
+      >
+        <Text className={styles.header_title}>ポケモンわかるかな?</Text>
+        <HamburgerIcon
+          boxSize={8}
+          // TODO: Menu の実装
+        />
+      </Box>
+      {/* Header の下に要素が隠れないようにするための placeholder */}
+      <Box h={12} display={"hidden"} />
+    </>
   );
 };
