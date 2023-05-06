f3242bc195a89119645ce921458c0d06aaba8fa0
ich5nkn <kogaktt@gmail.com>
2023-05-06 17:49:09 +0900

    feat: footer links

--- src/components/pages/index/Footer.tsx
+++ src/components/pages/index/Footer.tsx
@@ -1,4 +1,5 @@
 import { Box, Text } from "@chakra-ui/react";
+import Link from "next/link";
 
 export const Footer = () => {
   return (
@@ -12,15 +13,28 @@ export const Footer = () => {
       >
         Information
       </Text>
-      <Text color="white" mt={2} fontSize="sm" pl={2}>
-        利用規約
-      </Text>
-      <Text color="white" mt={2} fontSize="sm" pl={2}>
-        連絡先 ( Twitter )
-      </Text>
-      <Text color="white" mt={2} fontSize="sm" pl={2}>
-        GitHub
-      </Text>
+      {/* TODO: 利用規約ページへのリンク */}
+      <FooterItem name="利用規約" href="/" />
+      <FooterItem
+        name="連絡先 ( Twitter )"
+        href="https://twitter.com/ich5nkn"
+      />
+      <FooterItem
+        name="GitHub"
+        href="https://github.com/ich5nkn/pokemon-wakarukana"
+      />
     </Box>
+  );
+};
+
+interface FooterItemProps {
+  name: string;
+  href: string;
+}
+const FooterItem = ({ name, href }: FooterItemProps) => {
+  return (
+    <Text color="white" mt={2} fontSize="sm" pl={2}>
+      <Link href={href}>{name}</Link>
+    </Text>
   );
 };
