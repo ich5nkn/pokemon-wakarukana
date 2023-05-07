4ddf77fd1f441876d3a1b12dc511b0f9ebdf2aac
ich5nkn <kogaktt@gmail.com>
2023-05-07 18:27:20 +0900

    feat: loading spinner

--- src/components/pages/quiz/QuizImage.tsx
+++ src/components/pages/quiz/QuizImage.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import { Center, Spinner } from "@chakra-ui/react";
+import { Center } from "@chakra-ui/react";
+import { Spinner } from "./Spinner";
 
 interface OwnProps {
   loadingImg: boolean;
@@ -8,20 +9,21 @@ interface OwnProps {
 }
 
 export const QuizImage = ({ loadingImg, onLoadingComplete, src }: OwnProps) => {
-  if (!src) return null;
   return (
     <Center mx={"auto"} maxW="75%" my={4} h={264}>
-      <Spinner hidden={!loadingImg} size={"xl"} />
-      <Image
-        src={src}
-        alt="pokemon image"
-        width={264}
-        height={264}
-        unoptimized={true}
-        loading="eager"
-        onLoadingComplete={onLoadingComplete}
-        hidden={loadingImg}
-      />
+      {loadingImg && <Spinner />}
+      {src && (
+        <Image
+          src={src}
+          alt="pokemon image"
+          width={264}
+          height={264}
+          unoptimized={true}
+          loading="eager"
+          onLoadingComplete={onLoadingComplete}
+          hidden={loadingImg}
+        />
+      )}
     </Center>
   );
 };
