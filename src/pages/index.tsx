8e484e1bb567c70e901df915b578e0c2bd919096
ich5nkn <kogaktt@gmail.com>
2023-05-21 00:16:23 +0900

    feat: add information

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,21 +14,23 @@ export default function Home() {
         <TopView />
         <PlayButton />
         {/* TODO: ↓↓↓↓↓↓ この辺りに記述するコンテンツはまだ仮 ↓↓↓↓↓↓ */}
-        <Box bgColor={"gray.100"} borderRadius={4} p={4} mt={12}>
-          <Text
-            fontSize={"xl"}
-            textAlign={"left"}
-            fontWeight={700}
-            borderBottom={"1px solid"}
-          >
-            あそびかた
+
+        <Box mt={16} p={4} bgColor={"blue.100"} lineHeight={2}>
+          <Text fontSize={"lg"} fontWeight={700}>
+            このサイトについて
           </Text>
-          <Text></Text>
+          <Text mb={2}>
+            ポケモンの画像を見て名前を答える、シンプルなゲームが遊べるサイトです！
+          </Text>
+          <Text>
+            １９９６年に発売された赤・緑バージョンとともに１５１匹のポケモンが誕生し、現在では１，０００匹を超える多種多様なポケモンが存在しています。あなたはどれくらいポケモンの名前を答えられますか？
+            このサイトでは、ポケモンを全く知らない人から、毎日ポケモンで遊んでいる方まで楽しめる難易度がそろっています。自分で難易度を調節することもできます、ぜひ遊んでみてください！
+          </Text>
         </Box>
 
-        <Box bgColor={"gray.200"} borderRadius={10} h={300}>
-          <Text>コンタクト</Text>
-          <Text>ご意見・ご要望はこちらの Twitter まで @ich5nkn</Text>
+        <Box my={4} p={4} bgColor={"red.100"}>
+          <Text fontSize={"xl"}>ギャラリー</Text>
+          カルーセルでのプレイ画面のプレビューを実装
         </Box>
         {/* TODO: ↑↑↑↑↑↑ この辺りに記述するコンテンツはまだ仮 ↑↑↑↑↑↑ */}
         <Footer />
