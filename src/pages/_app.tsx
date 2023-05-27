f6fcc20de572a4b1b957a0678d5bf5bff414b1cf
ich5nkn <kogaktt@gmail.com>
2023-05-27 18:35:40 +0900

    feat: add ga4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,20 @@ import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BaseLayout } from "@/components/BaseLayout";
 import { GlobalContextProvider } from "@/hooks/useGlobalState";
+import { Analytics } from "@/components/Analytics";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <GlobalContextProvider>
-      <ChakraProvider>
-        <BaseLayout>
-          <Component {...pageProps} />
-        </BaseLayout>
-      </ChakraProvider>
-    </GlobalContextProvider>
+    <>
+      <GlobalContextProvider>
+        <ChakraProvider>
+          <BaseLayout>
+            <Component {...pageProps} />
+          </BaseLayout>
+        </ChakraProvider>
+      </GlobalContextProvider>
+      <Analytics />
+    </>
   );
 }
 
