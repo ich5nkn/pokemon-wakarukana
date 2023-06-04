20d22643549f54d84c4e97b3a2166e7ce048d85b
ich5nkn <kogaktt@gmail.com>
2023-06-04 16:47:54 +0900

    feat: draggable carousel

--- src/components/pages/index/Carousel.tsx
+++ src/components/pages/index/Carousel.tsx
@@ -1,5 +1,6 @@
 import { Image, Box, HStack } from "@chakra-ui/react";
 import style from "@/styles/carousel.module.css";
+import { useRef, useState } from "react";
 
 interface OwnProps {
   images: {
@@ -9,9 +10,43 @@ interface OwnProps {
 }
 
 export const Carousel = ({ images }: OwnProps) => {
+  const carouselRef = useRef<HTMLDivElement>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(0);
+
+  const onMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setIsDragging(true);
+    setStartX(e.pageX - (carouselRef.current?.offsetLeft || 0));
+    setScrollLeft(carouselRef.current?.scrollLeft || 0);
+  };
+
+  const onMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  const onMouseLeave = () => {
+    setIsDragging(false);
+  };
+
+  const onMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - (carouselRef.current?.offsetLeft || 0);
+    const walk = x - startX;
+    carouselRef.current!.scrollLeft = scrollLeft - walk;
+  };
+
   return (
     <Box>
-      <HStack className={style.carouselWrapper}>
+      <HStack
+        className={style.carouselWrapper}
+        ref={carouselRef}
+        onMouseDown={onMouseDown}
+        onMouseLeave={onMouseLeave}
+        onMouseUp={onMouseUp}
+        onMouseMove={onMouseMove}
+      >
         {images.map((image, i) => {
           return (
             <Image
